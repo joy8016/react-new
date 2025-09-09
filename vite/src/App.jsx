cf5a17323b3f025248563338bcf264fbf98f0caa
joy8016 <maityjoydeb67@gmail.com>
2025-09-09 21:54:46 +0530

    react

---
+++ vite/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function Components() {
+  return (
+    <div>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis non consequatur similique, officiis nobis est, dolorem, enim corrupti eaque labore libero veniam perspiciatis!</p>
+
+    </div>
+  )
+}
+
+function App() {
+
+
+  return (
+
+    <div>
+      
+      <h1>joydeb maity</h1>
+      <button>
+        click here <br />
+        {2 * 3}
+      </button>
+
+      <Components/>
+      <Components/>
+      <h2>hey</h2>
+    </div>
+
+
+  )
+}
+
+export default App
