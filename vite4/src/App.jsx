30b78f17a12b4406febab6bc0171cedac1f40d2e
joy8016 <maityjoydeb67@gmail.com>
2025-10-29 23:36:37 +0530

    jsx

---
+++ vite4/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+
+function App(){
+  return(
+
+    <>
+    
+    <h1 className="bg-red-950">kaise ho vai log</h1>
+    <button className='bg-amber-300  rounded-2xl'>click me</button>
+
+ 
+    </>
+  )
+}
+
+
+export default App
