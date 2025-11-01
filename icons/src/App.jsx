01964ea58625943b80df381a5163cbeb16cb84dd
joy8016 <maityjoydeb67@gmail.com>
2025-11-01 15:48:37 +0530

    jsx

---
+++ icons/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { FaFingerprint } from "react-icons/fa";
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1 >i want this<FaFingerprint style={{color:'red'}}  /></h1>
+    
+    </>
+  )
+}
+
+export default App
