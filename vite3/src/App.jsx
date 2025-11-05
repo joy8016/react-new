0d9df4cced552b636766a73c9712f3e2390db61b
joy8016 <maityjoydeb67@gmail.com>
2025-11-06 00:19:46 +0530

    jsx

---
+++ vite3/src/App.jsx
@@ -0,0 +1,20 @@
+import  './App.css'
+import Card from './card'
+
+ 
+function App(){
+  return(
+    <>
+
+
+    <h1>kaise ho vai log</h1>
+    <Card name="joydeb"/>
+    
+   
+  
+    
+    </>
+  )
+}
+
+export default App
---
+++ vite3/src/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const card = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+export default card
+
