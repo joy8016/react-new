1f67e7e6f15eda53d46aeaeec904d05b15e6b4df
joy8016 <maityjoydeb67@gmail.com>
2025-11-06 00:20:21 +0530

    jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,23 +1,101 @@
-
+import Header from './Header'
 import './App.css'
+import React from 'react'
 
 function App() {
-  let name = "joydeb maity"
+  //   let name = "joydeb maity"
 
-  function greet(){
-    return "how are you?";
-  }
-  let age = 129;
+  //   function greet(){
+  //     return "how are you?";
+  //   }
+  //   let age = 129;
+
+  // let arr = ["iron man","thor", "spider-man", "hulk"];
+
+
+
+  //assign it to variables
+
+  // let you = <h1>hello brother, this is {2+2} you</h1>
+
+
+
+
+const user ={
+  name:'joydeb maity',
+  age:23,
+}
+
+
 
   return (
     <>
-      <h1>hello {name} {greet()} {7+8} </h1>
+      {/* <h1>hello {name} {greet()} {7+8} </h1>
 
-      <h1>{age>=18?"you can drive": "you can't drive"}</h1>
+      <h1>{age>=18?"you can drive": "you can't drive"}</h1> */}
+
+      {/* {arr.map(function(ele){
+        return <h1>{ele}</h1>;
+      })} */}
+
+      {/* {you}; */}
+
+
+
+
+
+
+      {/* pass as props */}
+      {/* <Header name="joydeb" title="maity"/> */}
+
+
+
+
+      {/* use react.createElement */}
+
+      {/* {React.createElement('h1', {className: 'heading', style:{
+        backgroundColor:'red',
+      }}, 'hello')} */}
+
+
+      {/* {React.createElement('h1', {className: 'heading', style:{background:'red'}}, "Hello how are you")} */}
+
+
+      {/*       
+       <div>
+        <p >hello</p>
+       </div> */}
+
+      {/* {React.createElement('div', null, React.createElement('p', 'hello'))} */}
+
+
+
+      {/*  */}
+      {/* {React.createElement(
+        'h1',
+        {className: 'heading', style: {backgroundColor:'red', color:'black', fontSize:'30vh'}},
+        'hello, muskan'
+
+      )} */}
+
+
+{/* 
+      {React.createElement(
+        'div',
+        { className: 'card' },
+        
+        React.createElement('h1', null, `hello ${user.name}`),
+        React.createElement('p', null, `you are ${user.age +1} years old`)
+          
+
+
+      )} */}
+
+      {React.createElement('div', {className:'alpha' , style:{color:'red', fontSize:'200px', backgroundColor:'pink'}}, 'hello')}
+
 
     </>
   )
 }
 
 export default App
- 
---
+++ app/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header({name, title}){
+  return (
+    <div>
+        {name} {title}
+      
+    </div>
+  )
+}
+
+export default Header
